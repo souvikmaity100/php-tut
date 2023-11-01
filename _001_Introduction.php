46bbf14745e7c4c7d9a8c16df8cf4f1ca29bc944
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-11-01 12:30:53 +0530

    php introduction

---
+++ _001_Introduction.php
@@ -0,0 +1,56 @@
+<!-- What is PHP?
+PHP: Hypertext Preprocessor
+Create Dynamic Website with Database -->
+
+
+<!-- Why we learn PHP ?
+Ease of use and learn
+● Cost efficiency (open source)
+● Object Oriented Programming
+● Cross-platform compatibility
+● Works with All servers Apache, IIS etc.
+● Security
+● Huge Community -->
+
+
+<!-- Uses of PHP in Web Development :
+• CMS (Content Management System)
+• E-commerce Website
+• Mail server
+● Chatting System
+File Storage System
+● Enterprise Resource Planning
+● Customer Relationship Management -->
+
+
+<!-- Popular Websites in PHP :
+• Facebook
+• Yahoo
+● Wikipedia
+● Wordpress
+MailChimp
+• Flickr
+• Flipkart -->
+
+<!-- Software Requirement for PHP :
+• HTML Editor
+• Web Browser
+● Wamp or Xampp server -->
+
+<!-- Editor for PHP :
+● Notepad
+Notepad++
+• VS Code
+• Sublime
+• Atom
+● PhpStorm -->
+
+<!-- How to run PHP Code ?
+PHP MySQL Apache Server
+• WampServer
+XAMPP -->
+
+
+<?php
+    echo "Hello World";
+?>
