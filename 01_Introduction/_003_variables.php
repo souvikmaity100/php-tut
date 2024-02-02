c052e7565a70b3b8eefa4d55863e3b02ccea8554
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-02-02 21:40:00 +0530

    How to write a Variable Name

---
+++ 01_Introduction/_003_variables.php
@@ -0,0 +1,25 @@
+<?php
+
+    $name = "Souvik";
+    $address = "Subharara";
+    $pin = 711112;
+    $income = 200000;
+
+    echo "This guy name is $name, He lived in $address, $pin <br>";
+    echo "$name is a good boy <br>";
+    echo "His income is Rs. $income <br>";
+    
+?>
+
+<!-- How to write a Variable Name :
+Write Way           Wrong Way
+• $firstname        $first name
+• $_firstname       $99firstname
+• $first_name       $first%name
+• $first-name
+• $firstName
+• $firstname99 -->
+
+
+<!-- $age $AGE
+Not same -->
