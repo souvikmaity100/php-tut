7017ec29117de3b0dae73ba6073d903321c1360f
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-03-10 18:35:46 +0530

    string upper and lower case method

---
+++ 04_string_and_string_methods/_002_upperCase_lowerCase.php
@@ -0,0 +1,18 @@
+<?php
+
+    $msg = "hello World, a quick brown FOX";
+
+    echo strtolower($msg)."<br><br>";
+
+    echo strtoupper($msg)."<br><br>";
+
+    echo lcfirst($msg)."<br><br>";
+
+    echo ucfirst($msg)."<br><br>";
+
+    echo ucwords($msg)."<br><br>";
+
+
+
+
+?>
