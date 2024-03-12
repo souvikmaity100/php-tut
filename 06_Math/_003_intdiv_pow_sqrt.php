75f7e2fa19e3838ab7ad0ac35afc9d43cc915c3b
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-03-12 22:46:19 +0530

    intdiv(), pow(), and sqrt() in math function

---
+++ 06_Math/_003_intdiv_pow_sqrt.php
@@ -0,0 +1,28 @@
+<?php
+$dividend = 7;
+$divisor = 2;
+
+$quotient = intdiv($dividend, $divisor);
+
+echo $quotient;
+// Output: 3
+?>
+
+<?php
+$base = 2;
+$exponent = 3;
+
+$power = pow($base, $exponent);
+
+echo $power;
+// Output: 8
+?>
+
+<?php
+$number = 8;
+
+$square_root = sqrt($number);
+
+echo $square_root;
+// Output: 2.8284271247461903
+?>
