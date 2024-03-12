0acc021b009d78e86475601ccd0055df007d077d
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-03-12 22:25:09 +0530

    chr and ord, ASCII

---
+++ 05_hashing_and_other/_004_chr_and_ord.php
@@ -0,0 +1,17 @@
+<?php
+$ascii_value = 65;
+
+$character = chr($ascii_value);
+
+echo $character;
+// Output: A
+?>
+
+<?php
+$character2 = 'A';
+
+$ascii_value2 = ord($character2);
+
+echo $ascii_value2;
+// Output: 65
+?>
